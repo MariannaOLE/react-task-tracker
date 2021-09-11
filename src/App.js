d6d02fc042e776913e85288a972109fcae88a34e
MariannaOLE <marianna.olefyrenko@gmail.com>
2021-09-11 19:31:00 +0800

    Toggle reminder & conditional styling

--- src/App.js
+++ src/App.js
@@ -28,11 +28,24 @@ const App = () => {
     const deleteTask = (id) => {
       setTasks(tasks.filter((task) => task.id !==id))
     }
-    
+    // Toggle Reminder
+    const toggleReminder = (id) => {
+      setTasks(
+        tasks.map((task) => task.id === id ? { ...task, reminder:
+        !task.reminder } : task
+        )
+      )
+    }
+
   return (
     <div className="container">
         <Header/>
-        <Tasks tasks={tasks} onDelete={deleteTask}/>
+        {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}
+        />
+  ) : (
+       'No Tasks To Show'
+  )}
     </div>
   )
 }
