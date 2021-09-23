9610be5f6958636f5709c0dfe92b949e1c1b57e2
MariannaOLE <marianna.olefyrenko@gmail.com>
2021-09-23 11:25:37 +0800

    Add task submit

--- src/App.js
+++ src/App.js
@@ -28,21 +28,9 @@ const App = () => {
     
     // Add Task
     const addTask = async (task) => {
-      const res = await fetch('http://localhost:5000/tasks', {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(task),
-      })
-  
-      const data = await res.json()
-  
-      setTasks([...tasks, data])
-  
-      // const id = Math.floor(Math.random() * 10000) + 1
-      // const newTask = { id, ...task }
-      // setTasks([...tasks, newTask])
+      const id = Math.floor(Math.random() * 10000) + 1
+      const newTask = { id, ...task }
+      setTasks([...tasks, newTask])
     }
   
 
