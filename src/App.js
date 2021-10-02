0ce2da229864fe080e3b657cac125cd67b31d042
MariannaOLE <marianna.olefyrenko@gmail.com>
2021-10-02 11:56:40 +0800

    Add task submit

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const App = () => {
   return (
     <div className="container">
         <Header/>
-        <AddTask onAdd={AddTask} />
+        <AddTask onAdd={addTask} />
         {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}
         />
