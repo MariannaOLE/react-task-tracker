3c9daefaee60cc92027b3602f56412da269a0295
MariannaOLE <marianna.olefyrenko@gmail.com>
2021-10-04 10:16:23 +0800

    Button toggle

--- src/App.js
+++ src/App.js
@@ -51,8 +51,11 @@ const App = () => {
 
   return (
     <div className="container">
-        <Header onAdd={() => setShowAddTask(!showAddTask)} />
-        {showAddTask && <AddTask onAdd={addTask} />}
+        <Header onAdd={() => setShowAddTask(!showAddTask)} 
+        showAdd={showAddTask} 
+        />
+        {showAddTask && <AddTask onAdd={addTask}
+        />}
         {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}
         />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({title, onAdd }) => {
+const Header = ({title, onAdd, showAdd }) => {
    
     return (
         <header className='header'>
           <h1>{title}</h1>
-          <Button color='green' text='Add' onClick=
-          {onAdd} />
+          <Button 
+          color={showAdd ? 'red' : 'green'} 
+          text={showAdd ? 'Close' : 'Add'} 
+          onClick={onAdd} />
           
 
         </header>
